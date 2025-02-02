87828c9b6bef73f3b34f870be54da9e0f25471fd
CC007 <Coolcat_the_best2@Hotmail.com>
2025-02-02 22:58:25 +0100

    First attempt at defining an ancestry tree structure

---
+++ shared/src/commonMain/kotlin/nl/cqit/legasee/Graphs.kt
@@ -0,0 +1,56 @@
+package nl.cqit.legasee
+
+object AncestorTree {
+    data class Person(
+        val parents: List<ParentalFigure>,
+    )
+    data class ParentalFigure(
+        val parent: Person,
+        val type: ParentalFigureType,
+        val legalParent: Boolean,
+        val secondaryTypes: List<ParentalFigureType>,
+    )
+    sealed interface ParentalFigureType {
+        val type: String
+        data object Father : ParentalFigureType {
+            override val type: String = "father"
+        }
+        data object Mother : ParentalFigureType {
+            override val type: String = "mother"
+        }
+        data object BiologicalFather : ParentalFigureType {
+            override val type: String = "biological father"
+        }
+        data object BiologicalMother : ParentalFigureType {
+            override val type: String = "biological mother"
+        }
+        data object StepFather : ParentalFigureType {
+            override val type: String = "step father"
+        }
+        data object StepMother : ParentalFigureType {
+            override val type: String = "step mother"
+        }
+        data object Guardian : ParentalFigureType {
+            override val type: String = "guardian"
+        }
+        data object FosterParent : ParentalFigureType {
+            override val type: String = "foster parent"
+        }
+        data object AdoptiveFather : ParentalFigureType {
+            override val type: String = "adoptive father"
+        }
+        data object AdoptiveMother : ParentalFigureType {
+            override val type: String = "adoptive mother"
+        }
+        data object GodParent : ParentalFigureType {
+            override val type: String = "godparent"
+        }
+        data object SurrogateMother : ParentalFigureType {
+            override val type: String = "surrogate mother"
+        }
+        data object DonorParent : ParentalFigureType {
+            override val type: String = "donor parent"
+        }
+        interface CustomParentalFigureType : ParentalFigureType
+    }
+}
